9a07258a4f83a4111dfe8dfa67f49bb26a7533b0
tomguillermou <tomguillermou@gmail.com>
2019-07-19 11:58:47 +0200

    Update authenticateUser.ts

--- src/middlewares/authenticateUser.ts
+++ src/middlewares/authenticateUser.ts
@@ -1,38 +1,41 @@
 import { Request, Response, NextFunction } from "express";
-import { verify, VerifyErrors } from 'jsonwebtoken';
+import jwt from 'jsonwebtoken';
 import User from "../models/User";
 
 
 const JWT_SECRET = process.env.JWT_SECRET || 'secret';
 
 
-export default function authenticateUser(req: Request, res: Response, next: NextFunction) {
+export default async function authenticateUser(req: Request, res: Response, next: NextFunction) {
 
-  // Parse JWT from Bearer token in header
-  const bearerToken = req.header('authorization');
+  try {
+    const bearerToken = req.header('authorization');
 
-  if (bearerToken) {
-    const tokenArray = bearerToken.split(' ');
-    const token = tokenArray[1];
+    if (bearerToken === undefined) {
+      throw new Error('Missing bearer token')
+    }
 
-    verify(token, JWT_SECRET, (err: VerifyErrors, decodedToken: any) => {
-      if (err) {
-        console.log(err);
-        return res.status(500).send({ error: err })
-      }
+    // Parse encoded JWT from Bearer token
+    const encodedToken = bearerToken.split(' ')[1];
 
-      User.findById(decodedToken.userId, (err, user) => {
-        if (err) { return res.status(500).send({ error: err }) }
+    // The encoded token corresponds to the user id trying to log in
+    const decodedToken = jwt.verify(encodedToken, JWT_SECRET);
 
-        if (user === null) {
-          return res.status(401).json({ message: 'Authentication failed' });
-        }
-      })
+    // Check if decoded token is not a valid ObjectId
+    if (typeof decodedToken !== 'string' || !decodedToken.match(/^[0-9a-fA-F]{24}$/)) {
+      throw new Error('Invalid bearer token');
+    }
 
-      next();
-    });
+    const authUser = await User.findById(decodedToken).exec();
 
-  } else {
-    res.status(500).send({ error: 'Missing authorization header' });
+    if (authUser === null) {
+      throw new Error('Authentication failed');
+    }
+
+    next();
+
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error });
   }
-};
+}
