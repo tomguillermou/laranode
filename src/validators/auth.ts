d99e10d7c4513ce38d7ab2b5c1b44574f45d620f
tomguillermou <tomguillermou@gmail.com>
2019-07-25 18:23:25 +0200

    Fixing stupid mistake

--- src/validators/auth.ts
+++ src/validators/auth.ts
@@ -8,7 +8,7 @@ import errorMessage from "../config/errors/messages.json";
 export function login(req: Request, res: Response, next: NextFunction) {
 
   try {
-    if (isEmail(req.body.email)) { throw new Error(errorMessage.invalidEmailFormat); }
+    if (!isEmail(req.body.email)) { throw new Error(errorMessage.invalidEmailFormat); }
 
     next();
 
@@ -20,15 +20,15 @@ export function login(req: Request, res: Response, next: NextFunction) {
 export function register(req: Request, res: Response, next: NextFunction) {
 
   try {
-    if (isEmail(req.body.email)) { throw new Error(errorMessage.invalidEmailFormat); }
+    if (!isEmail(req.body.email)) { throw new Error(errorMessage.invalidEmailFormat); }
 
-    if (isLength(req.body.password, { min: 8 })) { throw new Error(errorMessage.passwordTooShort); }
+    if (!isLength(req.body.password, { min: 8 })) { throw new Error(errorMessage.passwordTooShort); }
 
-    if (matches(req.body.password, /\d/)) { throw new Error(errorMessage.passwordMissingDigit); }
+    if (!matches(req.body.password, /\d/)) { throw new Error(errorMessage.passwordMissingDigit); }
 
-    if (matches(req.body.password, /[a-z]/)) { throw new Error(errorMessage.passwordMissingLowercaseLetter); }
+    if (!matches(req.body.password, /[a-z]/)) { throw new Error(errorMessage.passwordMissingLowercaseLetter); }
 
-    if (matches(req.body.password, /[A-Z]/)) { throw new Error(errorMessage.passwordMissingUppercaseLetter); }
+    if (!matches(req.body.password, /[A-Z]/)) { throw new Error(errorMessage.passwordMissingUppercaseLetter); }
 
     next();
 
